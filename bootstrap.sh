58cacbe0cf83cbdb15f616cd9074dacfc0077415
lgmerino <lgmerino@yahoo.com>
2019-05-30 11:06:00 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,9 @@ pip install ipython
 pip install jupyter
 pip install pillow
 pip install mapbox
-# export MAPBOX_TOKEN = 'pk.eyJ1Ijoid29ya3Nob3BwaWxsb3ciLCJhIjoiY2p2NXFjbnhhMGxlYzN5cW9pdHM5OXhxNiJ9.GXBx6y5U1YOEr4isWJg0-Q'
 cd /vagrant
 mkdir /home/vagrant/.jupyter
 cp jupyter_notebook_config.py /home/vagrant/.jupyter/
 jupyter notebook --ip=0.0.0.0 --allow-root ./taller_notebook.ipynb
-# workshoppillow/work12pillow34
+
 
